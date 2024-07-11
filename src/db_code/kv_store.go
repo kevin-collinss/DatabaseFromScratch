6aed286dd2b9a61e8c441a9715e8550a1eec878f
Kevin Collins <kevinmscollins@gmail.com>
2024-07-11 20:41:29 +0100

    feat: added LogCreate and LogAppend functions

--- src/db_code/kv_store.go
+++ src/db_code/kv_store.go
@@ -29,6 +29,20 @@ func SaveData(path string, data []byte) error {
 	return os.Rename(tmp, path)
 }
 
+func LogCreate(path string) (*os.File, error) {
+	return os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0664)
+}
+
+func LogAppend(fp *os.File, line string) error {
+	buf := []byte(line)
+	buf = append(buf, '\n')
+	_, err := fp.Write(buf)
+	if err != nil {
+		return err
+	}
+	return fp.Sync()
+}
+
 func randomInt() int {
 	return rand.Intn(10000)
 }
