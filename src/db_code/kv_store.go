a22a70944960cc75fc896bb52bf989a0b056181e
Kevin Collins <kevinmscollins@gmail.com>
2024-07-11 20:31:44 +0100

    feat: improvement by adding temp storage and checking if issue occurs during write

--- src/db_code/kv_store.go
+++ src/db_code/kv_store.go
@@ -1,14 +1,28 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
 
 func SaveData(path string, data []byte) error {
-	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	tmp := fmt.Sprintf("%s.tmp.%d", path, randomInt())
+	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
 	if err != nil {
 		return err
 	}
 	defer fp.Close()
 
 	_, err = fp.Write(data)
-	return err
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
+
+	return os.Rename(tmp, path)
+}
+
+func randomInt() int {
+	return rand.Intn(10000)
 }
